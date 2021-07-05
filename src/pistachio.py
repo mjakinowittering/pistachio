0ceff51bc8b47216569c3c86687e77c29a5cd1d3
Matthew Akino-Wittering <matthew.akinowittering@outlook.com>
2021-07-05 21:44:11 +0100

    Work to add new methods with tests to confirm if a resource is a directory, file or symlink along with describing a resource.

--- src/pistachio.py
+++ src/pistachio.py
@@ -1,4 +1,18 @@
 from pathlib import Path
+import os
+
+
+def describe(path_str):
+    """
+    Method to describe the type of resources.
+    """
+    return {
+        'abspath': os.path.abspath(path_str),
+        'exists': exists(path_str),
+        'is_directory': is_directory(path_str),
+        'is_file': is_file(path_str),
+        'is_symlink': is_symlink(path_str)
+    }
 
 
 def exists(path_str):
@@ -6,3 +20,24 @@ def exists(path_str):
     Method to return True or False whether a resource exists.
     """
     return Path(path_str).exists()
+
+
+def is_directory(path_str):
+    """
+    Method to return True or False whether a resource is a directory.
+    """
+    return Path(path_str).is_dir()
+
+
+def is_file(path_str):
+    """
+    Method to return True or False whether a resource is a file.
+    """
+    return Path(path_str).is_file()
+
+
+def is_symlink(path_str):
+    """
+    Method to return True or False whether a resource is a symbolic link.
+    """
+    return Path(path_str).is_symlink()
--- tests/test_pistachio.py
+++ tests/test_pistachio.py
@@ -1,4 +1,49 @@
+from schema import Schema, And, Use, SchemaError
 from src import pistachio
+
+
+DESCRIBE_SCHEMA = Schema({
+    'abspath': And(Use(str)),
+    'exists': And(Use(bool)),
+    'is_directory': And(Use(bool)),
+    'is_file': And(Use(bool)),
+    'is_symlink': And(Use(bool))
+})
+
+
+def schema_validation(example, schema):
+    """
+    Method to confirm with a dictionary matches a schema.
+    """
+    try:
+        schema.validate(example)
+        return True
+    except SchemaError:
+        return False
+
+
+def test_describe_schema_directory():
+    """
+    Test to validate the describe method response for a directory.
+    """
+    example = pistachio.describe('tests')
+    assert schema_validation(example, DESCRIBE_SCHEMA) is True
+
+
+def test_describe_schema_file():
+    """
+    Test to validate the describe method response for a file.
+    """
+    example = pistachio.describe('README.md')
+    assert schema_validation(example, DESCRIBE_SCHEMA) is True
+
+
+def test_describe_schema_symlink():
+    """
+    Test to validate the describe method response for a symbolic link.
+    """
+    example = pistachio.describe('README.rst')
+    assert schema_validation(example, DESCRIBE_SCHEMA) is True
 
 
 def test_exists_true():
@@ -13,3 +58,31 @@ def test_exists_false():
     Test to confirm the exists method returns False.
     """
     assert pistachio.exists('LICENSE.md') is False
+
+
+def test_is_directory_true():
+    """
+    Test to confirm the is_directory method returns True.
+    """
+    assert pistachio.is_directory('tests') is True
+
+
+def test_is_directory_false():
+    """
+    Test to confirm the is_directory method returns False.
+    """
+    assert pistachio.is_directory('LICENSE.md') is False
+
+
+def test_is_file_true():
+    """
+    Test to confirm the is_file method returns True.
+    """
+    assert pistachio.is_file('README.md') is True
+
+
+def test_is_file_false():
+    """
+    Test to confirm the is_file method returns False.
+    """
+    assert pistachio.is_file('tests') is False
