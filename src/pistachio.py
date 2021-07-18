7ab5beec9e3c025f77891c548e9344bf057301ff
Matthew Akino-Wittering <matthew.akinowittering@outlook.com>
2021-07-18 15:23:28 +0100

    Updates to return the MD5 string of a file.

--- src/pistachio.py
+++ src/pistachio.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 
 import errno
+import hashlib
 import os
 
 
@@ -10,12 +11,13 @@ def describe(path_str):
     Method to describe the type of resources.
     """
     return {
-        'abspath': os.path.abspath(path_str),
-        'exists': exists(path_str),
-        'is_directory': is_directory(path_str),
-        'is_file': is_file(path_str),
-        'is_symlink': is_symlink(path_str),
-        'name': path_str.split('/')[-1]
+        "abspath": os.path.abspath(path_str),
+        "exists": exists(path_str),
+        "is_directory": is_directory(path_str),
+        "is_file": is_file(path_str),
+        "is_symlink": is_symlink(path_str),
+        "name": path_str.split("/")[-1],
+        "md5": "CHEESE"
     }
 
 
@@ -24,6 +26,23 @@ def exists(path_str):
     Method to return True or False whether a resource exists.
     """
     return Path(path_str).exists()
+
+
+def get_md5_hash(path_str):
+    """
+    Method to return the MD5 hash of a file.
+    """
+    if exists(path_str) is True:
+        md5_hash = hashlib.md5()
+        with open(path_str, "rb") as fh:
+            for block in iter(lambda: fh.read(4096), b""):
+                md5_hash.update(block)
+            fh.close()
+        return md5_hash.hexdigest()
+    else:
+        raise FileNotFoundError(
+            errno.ENOENT, os.strerror(errno.ENOENT), path_str
+        )
 
 
 def is_directory(path_str):
@@ -53,7 +72,7 @@ def touch(path_str):
     """
     if exists(path_str) is False:
         try:
-            open(path_str, 'a').close()
+            open(path_str, "a").close()
             return True
         except FileNotFoundError:
             raise FileNotFoundError(
